24c8d74d4484600dc39533ed932402ae1ea62675
LeonardoSouzaBento <bentosouzaleonardo@gmail.com>
2025-11-27 16:24:00 -0300

    style: correção da posição dos textos da hero.

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -9,15 +9,13 @@ const Hero = () => {
   return (
     <section
       className={`relative w-full h-[97vh] sm:h-[94vh] max-h-[920px]
-        flex items-end justify-center min-[470px]:items-center
-        overflow-hidden `}
+        flex items-center justify-center overflow-hidden `}
     >
       <FloatingElements />
       <HeroBackground />
 
       <div className="relative z-6 section-container p-0 text-center text-white">
-        <div className={`max-w-4xl mx-auto space-y-6 slide-up px-2.5 mb-12 
-          min-[380px]:mb-18 min-[470px]:mb-0 min-[470px]:mt-14`}>
+        <div className={`max-w-4xl mx-auto space-y-6 slide-up px-2.5 mt-20`}>
           <HeroTitle />
           <HeroButton />
         </div>
